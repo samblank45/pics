d3e52bab001388a8809f59ba3be758ccef9659de
sam <sblan45@gmail.com>
2020-12-10 22:47:46 -0500

    adds callbacks in children

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -16,7 +16,7 @@ class SearchBar extends React.Component {
 
   onFormSubmit(event) {
     event.preventDefault()
-    console.log(this.state.term)
+    this.props.onSubmit(this.state.term)
   }
 
   render() {
